222f1a0840e51cb45c8defa97864c28d5c7ce34c
TotalChen <totalatot@gmail.com>
2020-07-08 01:18:09 +0800

    Update update.sh

--- update.sh
+++ update.sh
@@ -1,2 +1,8 @@
-git pull origin
+gco master
+git pull origin master
+gco total
+git rebase master
+#rm -rf ./tmp;
+#sed -i 's/\"#src-git\"/\"src-git\"/g' ./feeds.conf.default;
+#./scripts/feeds clean;
 ./scripts/feeds update -a && ./scripts/feeds install -a
